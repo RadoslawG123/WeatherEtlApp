878adc745cee537be2be230b6721f6efb66a5f88
RadoslawG123 <gorecki208@onet.pl>
2025-10-02 16:21:17 +0200

    Craete .env file to store safely api_key. Install dotenv library for secure api_key. Adding some lines to Extract.py for testing.

--- Extract.py
+++ Extract.py
@@ -1,3 +1,10 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-req = requests.get("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
+def configure():
+    load_dotenv()
+
+def extract():
+    city_name = ""
+    req = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv('API_KEY')}")
