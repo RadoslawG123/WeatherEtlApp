c05a217d855326e62aa2e7d844912f30d7b5b0cd
RadoslawG123 <gorecki208@onet.pl>
2025-10-06 11:21:56 +0200

    Change request using units=metric for temperature in degrees Celcius. Create notes as a comment in extract.py file which descriping what data I want to gain from request.

--- Extract.py
+++ Extract.py
@@ -3,7 +3,15 @@ import os
 import json
 
 def extract(city_name):
-    req = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv('API_KEY')}")
-    print(req)
-    print(json.dumps(req.json(), indent=3))
-    print(type(req.json()))
+    raw_req = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv('API_KEY')}&units=metric")
+    json_req = json.dumps(raw_req.json(), indent=3)
+
+    # NOTES
+    # weather.main, weather.description, 
+    # main.temp, main.feels_like, main.pressure,
+    # wind.speed, wind.deg (Calculate in which direcion by myself), 
+    # clouds.all, clouds.name
+
+    # print(req)
+    # print(json.dumps(req.json(), indent=3))
+    # print(type(req.json()))
